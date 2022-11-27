824ee0737315be46dfb27a3b10b68454e837a419
David <David@DESKTOP-IS7IOOP>
2022-11-27 21:22:26 +0100

    consola

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,4 +1,20 @@
 package org.iesalandalus.programacion.reinaajedrez;
 public class Consola {
+
+	private Consola() {
+	}
+	      
+	public void mostrarMenu()
+    {
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("Menú para crear y/o mover a la reina");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.println("1.- Crear Reina.");
+        System.out.println("2.- Crear reina eligiendo color.");
+        System.out.println("3.- Mover.");
+        System.out.println("0.- Cerrar.");
+        System.out.println("");
+    }
 }
 
