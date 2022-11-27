7d07e41b367fac820a61ed00192152cb577ea7a0
David <David@DESKTOP-IS7IOOP>
2022-11-27 20:52:20 +0100

    Color2

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.reinaajedrez;
+
+public enum Color {
+	BLANCO("Blanco"), NEGRO("Negro");
+	private String color;
+
+	private Color(String color) {
+		this.color = color;
+	}
+}
